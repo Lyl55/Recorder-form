8fe1568a71171af9219c5ae8508567374b285d05
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2023-02-17 00:02:17 +0400

    first commit

---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recorder
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp;
+        Graphics gr;
+
+        string outputPath = "";
+        bool pathSelected = false;
+        string finalVidName = "FinalVideo.mp4";
+        ScreenRecorder screenRec = new ScreenRecorder(new Rectangle(), "");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool containsMP4 = finalVidName.Contains(".mp4");
+
+            if (pathSelected && containsMP4)
+            {
+                screenRec.setVideoName(finalVidName);
+                timer1.Start();
+            
+            }
+
+            else
+            {
+               MessageBox.Show("You must select video name that ends in '.mp4' " +
+                                "and you must select an output path", "Error");
+                finalVidName = "FinalVideo.mp4";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse("192.168.43.155"), 9095));
+            MessageBox.Show("Sent");
+            screenRec.Stop();
+            while (true)
+            {
+                var buffer = File.ReadAllBytes(@"C:\Test\video.mp4");
+                socket.Send(buffer);
+                Thread.Sleep(1000);
+                timer1.Stop();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                bmp = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+                gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(0, 0, 0, 0, new Size(bmp.Width, bmp.Height));
+                pictureBox1.Image = bmp;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                screenRec.RecordVideo();
+                label1.Text = screenRec.getElapsed();
+            }
+            catch (Exception mes)
+            {
+                MessageBox.Show(mes.Message);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse("192.168.43.155"), 9095));
+            MessageBox.Show("Connected");
+        }
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.Description = "Select an Output Folder";
+
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                outputPath = @folderBrowser.SelectedPath;
+                pathSelected = true;
+
+                Rectangle bounds = Screen.FromControl(this).Bounds;
+                screenRec = new ScreenRecorder(bounds, outputPath);
+            }
+            else
+            {
+                MessageBox.Show("Please select an output folder.", "Error");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ScreenRecorder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Video.FFMPEG;
+
+namespace Recorder
+{
+    class ScreenRecorder
+    {
+        private Rectangle bounds;
+        private string outputPath = "";
+        private string tempPath = "";
+        private int fileCount = 1;
+        private List<string> inputImageSequence = new List<string>();
+        private string videoName = "video.mp4";
+        private string finalName = "FinalVideo.mp4";
+
+
+        Stopwatch watch = new Stopwatch();
+        public ScreenRecorder(Rectangle b, string outPath)
+        {
+
+            CreateTempFolder("tempScreenCaps");
+
+            bounds = b;
+            outputPath = outPath;
+        }
+
+
+        private void CreateTempFolder(string name)
+        {
+            if (Directory.Exists("D://"))
+            {
+                string pathName = $"D://{name}";
+                Directory.CreateDirectory(pathName);
+                tempPath = pathName;
+            }
+            else
+            {
+                string pathName = $"C://Documents//{name}";
+                Directory.CreateDirectory(pathName);
+                tempPath = pathName;
+            }
+        }
+
+
+        public void setVideoName(string name)
+        {
+            finalName = name;
+        }
+
+
+        public string getElapsed()
+        {
+            return string.Format("{0:D2}:{1:D2}:{2:D2}", watch.Elapsed.Hours, watch.Elapsed.Minutes, watch.Elapsed.Seconds);
+        }
+
+
+        public void RecordVideo()
+        {
+
+            watch.Start();
+
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+
+                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+                }
+
+                string name = tempPath + "//screenshot-" + fileCount + ".png";
+                bitmap.Save(name, ImageFormat.Png);
+                inputImageSequence.Add(name);
+                fileCount++;
+                bitmap.Dispose();
+            }
+        }
+        private void SaveVideo(int width, int height, int frameRate)
+        {
+            int c = 0;
+            using (VideoFileWriter vFWriter = new VideoFileWriter())
+            {
+                vFWriter.Open(outputPath + "//" + videoName, width, height, frameRate, VideoCodec.MPEG4);
+                foreach (string imageLocation in inputImageSequence)
+                {
+                    Bitmap imageFrame = System.Drawing.Image.FromFile(imageLocation) as Bitmap;
+                    vFWriter.WriteVideoFrame(imageFrame);
+                    imageFrame.Dispose();
+                }
+                vFWriter.Close();
+                File.Copy(Path.Combine(outputPath, videoName), @"C:\Test\video.mp4");
+            }
+        }
+
+        public void Stop()
+        {
+            watch.Stop();
+            int width = bounds.Width;
+            int height = bounds.Height;
+            int frameRate = 10;
+            SaveVideo(width, height, frameRate);
+        }
+    }
+}
